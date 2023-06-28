cfb422f0b434a71a1f3adf7ea08225aea3b543c5
ki_hwan <117561565+KiHwanY@users.noreply.github.com>
2023-06-29 01:52:32 +0900

    Update HomeController.java

--- trakker/src/main/java/com/example/trakker/HomeController.java
+++ trakker/src/main/java/com/example/trakker/HomeController.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 
