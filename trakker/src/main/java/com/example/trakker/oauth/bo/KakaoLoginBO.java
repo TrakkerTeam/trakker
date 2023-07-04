31f6675ce5c08af436b28ba42998c3e5f1239563
KiHwan Yun <drg2524@naver.com>
2023-07-04 20:24:02 +0900

    MEMBER IMG CONFIG AND OAuth File commit

--- trakker/src/main/java/com/example/trakker/oauth/bo/KakaoLoginBO.java
+++ trakker/src/main/java/com/example/trakker/oauth/bo/KakaoLoginBO.java
@@ -19,33 +19,33 @@ public class KakaoLoginBO {
     private final static String KAKAO_CLIENT_SECRET = "Ny8gt87juxwA6Q18Gc3rryOvCBEu0lk0";
     private final static String KAKAO_REDIRECT_URI = "http://localhost:9090/trakker/callbackKakao";
     private final static String SESSION_STATE = "kakao_oauth_state";
-    /* 프로필 조회 API URL */
+
     private final static String PROFILE_API_URL ="https://kapi.kakao.com/v2/user/me";
 
 
-    /* 네아로 인증  URL 생성  Method */
+
     public String getAuthorizationUrl(HttpSession session) {
 
-        /* 세션 유효성 검증을 위하여 난수를 생성 */
+
         String state = generateRandomString();
-        /* 생성한 난수 값을 session에 저장 */
+
         setSession(session,state);
 
-        /* Scribe에서 제공하는 인증 URL 생성 기능을 이용하여 네아로 인증 URL 생성 */
+
         OAuth20Service oauthService = new ServiceBuilder()
                 .apiKey(KAKAO_CLIENT_ID)
                 .apiSecret(KAKAO_CLIENT_SECRET)
                 .callback(KAKAO_REDIRECT_URI)
-                .state(state) //앞서 생성한 난수값을 인증 URL생성시 사용함
+                .state(state)
                 .build(KakaoLoginApi.instance());
 
         return oauthService.getAuthorizationUrl();
     }
 
-    /* 네아로 Callback 처리 및  AccessToken 획득 Method */
+
     public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException {
 
-        /* Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인 */
+
         String sessionState = getSession(session);
         if(StringUtils.pathEquals(sessionState, state)){
 
@@ -56,29 +56,29 @@ public class KakaoLoginBO {
                     .state(state)
                     .build(KakaoLoginApi.instance());
 
-            /* Scribe에서 제공하는 AccessToken 획득 기능으로 네아로 Access Token을 획득 */
+
             OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
             return accessToken;
         }
         return null;
     }
 
-    /* 세션 유효성 검증을 위한 난수 생성기 */
+
     private String generateRandomString() {
         return UUID.randomUUID().toString();
     }
 
-    /* http session에 데이터 저장 */
+
     private void setSession(HttpSession session,String state){
         session.setAttribute(SESSION_STATE, state);
     }
 
-    /* http session에서 데이터 가져오기 */
+
     private String getSession(HttpSession session){
         return (String) session.getAttribute(SESSION_STATE);
     }
 
-    /* Access Token을 이용하여 네이버 사용자 프로필 API를 호출 */
+
     public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException{
 
         OAuth20Service oauthService =new ServiceBuilder()
@@ -90,9 +90,9 @@ public class KakaoLoginBO {
         oauthService.signRequest(oauthToken, request);
         Response response = request.send();
 
-        // 응답 데이터 확인
+
         String responseBody = response.getBody();
-        System.out.println("Kakao API Response: " + responseBody);
+
 
         return response.getBody();
     }
