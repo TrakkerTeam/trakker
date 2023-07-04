7af7d30d679ddac18cb05cbf849463ca508fa40e
yungyu kim <dbsrb15@gamil.com>
2023-07-04 17:03:44 +0900

    trip,faq 커밋

---
+++ trakker/src/main/java/com/example/trakker/item/AttachDTO.java
@@ -0,0 +1,10 @@
+package com.example.trakker.item;
+
+import lombok.Data;
+
+
+@Data
+public class AttachDTO {
+    private String fullName;
+    private Long t_num;
+}
--- trakker/src/main/java/com/example/trakker/item/RatingDTO.java
+++ trakker/src/main/java/com/example/trakker/item/RatingDTO.java
@@ -1,17 +1,12 @@
 package com.example.trakker.item;
 
 import lombok.Data;
-import lombok.ToString;
 
 @Data
-@ToString
 public class RatingDTO {
-    private long t_num; // 시퀀스
-    private long mem_num; // 회원 번호
-    private long review_num; // 리뷰 번호
-    private Double rating; // 별점
-
-    private Double ratingAvg; // 별점 총합
-
-
+    private long mem_num;
+    private long t_num;
+    private long review_num;
+    private Double rating;
+    private Double ratingAvg;
 }
